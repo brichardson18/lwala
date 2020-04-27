74f3f9e6abab11018108467db3945c6121ceb941
Aleksa Krolls <aleksagk@gmail.com>
2020-04-26 21:20:00 -0700

    New change example
    When you edit a file, you can select to *create a new branch* for the commit to "fork" your changes and ensure you're not updating the live master branch (linked to your OpenFn jobs)

--- commcare-salesforce-jobs/CHW-Reassignment-Production.js
+++ commcare-salesforce-jobs/CHW-Reassignment-Production.js
@@ -1,5 +1,6 @@
 upsert("Household__c", "CommCare_Code__c", fields(
   field("CommCare_Code__c", dataValue("form.case.@case_id")),
   field("Household_CHW__c", dataValue("form.CHW_ID")),
+  //new line example
   relationship("Area__r", "CommCare_User_ID__c", dataValue("form.area"))
 ));
